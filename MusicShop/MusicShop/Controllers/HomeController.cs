08c24a3fad168ba52bbf628885ce8834e4f30e7b
DamianLikszo <damian.likszo@gmail.com>
2017-10-29 01:29:43 +0200

    Modele + Baza + Inicjalizatory

--- MusicShop/MusicShop/Controllers/HomeController.cs
+++ MusicShop/MusicShop/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using MusicShop.DAL;
+using MusicShop.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,13 +10,18 @@ namespace MusicShop.Controllers
 {
     public class HomeController : Controller
     {
+        private StoreContext db = new StoreContext();
+
         // GET: Home
         public ActionResult Index()
         {
-            // Dodane zmiany
-            return View();
+            //Genre newGenre = new Genre { Name = "Rock", Description = "Opis gatunku", IconFilename = "1.png" };
+            //db.Genres.Add(newGenre);
+            //db.SaveChanges();
 
-            //Logowanie
+            var genresList = db.Genres.ToList();
+
+            return View();
         }
 
         public ActionResult StaticContent(string viewname)
---
+++ MusicShop/MusicShop/Models/Genre.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MusicShop.Models
+{
+    public class Genre
+    {
+        public int GenreId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string IconFilename { get; set; }
+
+        public ICollection<Album> Albums { get; set; }
+    }
+}
