5a0791a2fdbcea3b1da151a524b5f5072dbba3cd
DamianLikszo <damian.likszo@gmail.com>
2017-11-01 14:09:33 +0100

    widoki rejestracji i logowania

---
+++ MusicShop/MusicShop/ViewModels/AccountViewModels.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MusicShop.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage = "Musisz wprowadzić e-mail")]
+        [Display(Name = "Email")]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Musisz wprowadzić hasło")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Hasło")]
+        public string Password { get; set; }
+
+        [Display(Name = "Zapamiętaj mnie")]
+        public bool RememberMe { get; set; }
+    }
+
+    public class RegisterViewModel
+    {
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "Długość {0} musi wynosić conajmniej {2} znaków.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Hasło")]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Potwierdź hasło")]
+        [Compare("Password", ErrorMessage = "Hasła nie są zgodne.")]
+        public string ConfirmPassword { get; set; }
+    }
+}
